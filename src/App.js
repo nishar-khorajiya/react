20ee8e6e439f79d7bf20290b9599fc0d12d91175
Nishar Khorajiya <nisharkhorajiya9510@gmail.com>
2023-05-07 00:53:42 +0530

    video 13(mycode)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import React,{useState} from 'react';
+import TextForm from './components/TextForm';
 
 function App() {
+
+  const [mode,setmode]=useState('dark')
+  const [alert,setalert]=useState(null)
+
+  const showalert=(message,type)=>{
+    setalert({
+      msg:message,
+      type:type
+    })
+  }
   return (
     <>
-  <Navbar title="Nishar" abouttext="about us"></Navbar> 
+  <Navbar mode={mode} title="Nishar" abouttext="about us"></Navbar>
+
+  <Alert alert={alert}></Alert> 
+
   <div className='container my-3'>
-  {/* <TextForm heading="Enter the text to analyze"></TextForm> */}
+  <TextForm heading="Enter the text to analyze"></TextForm>
   <About></About>
   </div>
  
